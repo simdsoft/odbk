264ed03071fc6330d633a27c4e5754606f8a14e7
halx99 <halx99@live.com>
2019-05-11 10:49:07 +0800

    Add clang-format

--- odbk/odbk_core.h
+++ odbk/odbk_core.h
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <set>
 #include "odbk_def.h"
 #include "yasio/detail/singleton.h"
 using namespace yasio::gc;
@@ -13,6 +15,12 @@ enum
   APPF_UNITY    = 1 << 1,
   APPF_X86      = 1 << 2,
   APPF_X64      = 1 << 3,
+};
+
+struct bpinfo
+{
+    int type; // NORMAL, CONDITION, LOGPOINT
+    std::string info; // Can be empty, conditon expression, message
 };
 
 class odbk_core
@@ -33,12 +41,14 @@ public:
 
   std::string tools_wr_path_; // AppData\Local\x-studio365\, ldb_predef_modules.csv
 
+  // Windows ONLY
   void* app_inst_; // HANDLE
   void* app_module_; // HMODULE GetModuleHandleW(nullptr)
   void* app_process_; // HANDLE GetCurrentProcess
 
 private:
   /// Private members
+  std::unordered_map<int, std::set<std::string>> bpinfos_;
 };
 
 extern "C" {
